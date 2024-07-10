c6350dee576f1d43493cc51a59a9915bef2103f6
Paul Morris <paulh.morris@gmail.com>
2024-07-10 15:03:42 -0500

    Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import { vercelPreset } from '@vercel/remix/vite';
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+
+const isVercel = process.env.VERCEL;
 
 export default defineConfig({
   plugins: [
     remix({
+      presets: isVercel ? [vercelPreset()] : [],
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
